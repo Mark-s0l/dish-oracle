fa210e501a64a765ea8e95cda2af82e69b81699f
Marksol <solodkovsasha.2005@gmail.com>
2025-07-04 16:02:48 +0300

    feat(views): implement ProductsView to display product list with filtering by taste tag

--- food_hub/views.py
+++ food_hub/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from .models import Product
+
+
+class ProductsView(TemplateView):
+    template_name = "food_hub/product_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = Product.objects.prefetch_related("ratings__taste_tags")
+        tag_slug = self.kwargs.get("slug")
+        if tag_slug:
+            products = products.filter(ratings__taste_tags__slug=tag_slug)
+        context["products"] = products
+        return context
