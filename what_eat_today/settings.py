dabbefa58d1371339811b43b22d38f38af8b3d45
Marksol <solodkovsasha.2005@gmail.com>
2025-07-08 22:36:55 +0300

    chore(settings): add EAN_DB_API_URL and EAN_DB_JWT to settings.py

--- what_eat_today/settings.py
+++ what_eat_today/settings.py
@@ -10,12 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(DEBUG=(bool, False))
@@ -25,13 +23,14 @@ environ.Env.read_env(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost"])
 
+EAN_DB_API_URL = env('EAN_DB_API_URL')
+EAN_DB_JWT = env('EAN_DB_JWT')
 
 # Application definition
 
