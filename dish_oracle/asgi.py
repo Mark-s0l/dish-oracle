57c124c85b3e40f154564881a2239ec084f00c06
Marksol <solodkovsasha.2005@gmail.com>
2025-07-25 17:50:56 +0300

    chore(dish_oracle): update settings paths

--- dish_oracle/asgi.py
+++ dish_oracle/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for what_eat_today project.
+ASGI config for dish_oracle project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'what_eat_today.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dish_oracle.settings')
 
 application = get_asgi_application()
