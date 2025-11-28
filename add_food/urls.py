8cf3c766985001540b60dc89ee569d7105f2191c
Marksol <solodkovsasha.2005@gmail.com>
2025-11-28 16:23:27 +0300

    style(add_food): clean up imports in urls.py

--- add_food/urls.py
+++ add_food/urls.py
@@ -1,7 +1,8 @@
-from add_food import views
 from django.urls import path
 
-app_name = 'add_food'
+from add_food import views
+
+app_name = "add_food"
 
 urlpatterns = [
     path("", views.AddProductView.as_view(), name="add_product"),
