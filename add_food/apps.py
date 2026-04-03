1f2967abf3f73304476fffa67ed2a22949a4035f
Marksol <solodkovsasha.2005@gmail.com>
2026-04-03 19:11:03 +0300

    feat(apps): add url and API token verification before launching the application

--- add_food/apps.py
+++ add_food/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
 
 
 class AddFoodConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "add_food"
+
+    def ready(self):
+        from django.conf import settings
+        if not settings.EAN_DB_JWT or not settings.EAN_DB_API_URL:
+            raise ImproperlyConfigured("Не заданы параметры EAN_DB_JWT или EAN_DB_API_URL")
+        elif settings.EAN_DB_API_URL[-1] != "/":
+            raise ImproperlyConfigured("Неправильно задан параметр EAN_DB_API_URL - Отстствует '/' в конце строки")
