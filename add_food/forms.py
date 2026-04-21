a721a0dceeeeabe0ec9120625bc37d017cfa8cde
Marksol <solodkovsasha.2005@gmail.com>
2026-04-21 19:47:12 +0300

    fix(add_food): add validation of the product adding form

--- add_food/forms.py
+++ add_food/forms.py
@@ -1,15 +1,16 @@
 from django import forms
 
-from food_hub.models import Product
+from food_hub.models import Product, valid_ean13
 
 
 class AddProductForm(forms.Form):
     ean_code = forms.CharField(
+        validators=[valid_ean13],
         widget=forms.TextInput(
             attrs={
                 "placeholder": "Введите штрих-код 13 цифр",
                 "inputmode": "numeric",
                 "enterkeyhint": "search",
             }
-        )
+        ),
     )
