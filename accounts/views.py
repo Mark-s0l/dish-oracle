fe13c7ceb38578d6a8e05971d3340f85dfb58273
Marksol <solodkovsasha.2005@gmail.com>
2026-04-23 19:59:03 +0300

    feat(accounts): add user authentication with profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from accounts.models import CustomUser
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-
 class UserProfileView(LoginRequiredMixin, TemplateView):
     template_name = "accounts/user_profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user_info"] = self.request.user   
+        return context
+        
+    
