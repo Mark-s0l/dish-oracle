cf5866e809d7647dd18fa45cb3b0920db57e4fd7
Marksol <solodkovsasha.2005@gmail.com>
2026-05-02 21:36:33 +0300

    feat: add Redis container and configure Django cache backend

--- dish_oracle/settings.py
+++ dish_oracle/settings.py
@@ -106,6 +106,19 @@ DATABASES = {
     }
 }
 
+# Redis
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': env('REDIS_URL'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
