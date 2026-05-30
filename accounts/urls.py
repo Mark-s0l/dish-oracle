ca12db156a8a1f1d0852376aaf96d0a9a089d52a
Marksol <solodkovsasha.2005@gmail.com>
2026-05-30 18:55:19 +0300

    feat: add change_password and verification URL routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.UserProfileView.as_view(), name='profile'),
+    path('change_password/', views.ChangePasswordView.as_view(), name='change_password'),
+    path('change_password/verification', views.VerificationChangePassword.as_view(), name='verification_change_password')
 ]
