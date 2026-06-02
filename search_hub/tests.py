817bbce71ceef3a81fa1f16a7d20345aec8e1684
Marksol <solodkovsasha.2005@gmail.com>
2026-06-02 20:45:39 +0300

    tests(search_hub): remove local user fixture in favor of global conftest

--- search_hub/tests.py
+++ search_hub/tests.py
@@ -1,19 +1,7 @@
 import pytest
-from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 import food_hub.models as models
-
-User = get_user_model()
-
-
-@pytest.fixture(scope="module")
-def user(django_db_blocker):
-    with django_db_blocker.unblock():
-        user = User.objects.create_user(username="testuser", password="testpass")
-    yield user
-    with django_db_blocker.unblock():
-        user.delete()
 
 
 @pytest.fixture
