2f122caaf7bfe332c47d1d4f8931cdbdca98c578
Marksol <solodkovsasha.2005@gmail.com>
2026-06-10 16:48:43 +0300

    test(accounts): extract ViewBaseMixin, add base tests for ChangePasswordView
    - add UserFactory via factory_boy
    - move shared view test logic into ViewBaseMixin
    - migrate TestUserProfileView to use the mixin
    - add accessibility/template/form tests for TestChangePasswordView
    - switch to UserFactory, add refresh_from_db in setUp

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,39 +1,69 @@
 from unittest.mock import patch
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import PasswordChangeForm
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth import get_user_model
-from django.conf import settings
-from accounts.forms import ChangeEmailUser
 
+from accounts.forms import ChangeEmailUser
+from accounts.tests.factories import UserFactory
 
 User = get_user_model()
 
-class TestUserProfileView(TestCase):
+
+class ViewBaseMixin:
+    url = None
+    template_name = None
+    form_class = None
+    login_url_name = "accounts:login"
 
     @classmethod
     def setUpTestData(cls):
-        cls.url = reverse("accounts:profile")
-        cls.user = User.objects.create_user(username="testuser", password="testpassword123")
+        cls.user = UserFactory()
+        super().setUpTestData()
 
     def setUp(self):
+        self.user.refresh_from_db()
         self.client.force_login(self.user)
 
     def test_url_success(self):
-        response = self.client.get(self.url) 
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
 
-    def test_unauth_user_url_request(self):
+    def test_unauth_user_request(self):
         self.client.logout()
-        response = self.client.get(self.url) 
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, f"{reverse('accounts:login')}?next={self.url}")
+        self.assertRedirects(
+            response, f"{reverse(self.login_url_name)}?next={self.url}"
+        )
 
     def test_correct_template_used(self):
-        response = self.client.get(self.url) 
-        self.assertTemplateUsed(response, template_name="accounts/user_profile.html")
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, template_name=self.template_name)
 
     def test_correct_form_used(self):
-        response = self.client.get(self.url) 
-        self.assertIsInstance(response.context['form'], ChangeEmailUser)
+        response = self.client.get(self.url)
+        self.assertIsInstance(response.context["form"], self.form_class)
 
-    
+
+class TestUserProfileView(ViewBaseMixin, TestCase):
+    template_name = "accounts/user_profile.html"
+    form_class = ChangeEmailUser
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.url = reverse("accounts:profile")
+
+
+class TestChangePasswordView(ViewBaseMixin, TestCase):
+
+    template_name = "accounts/change_password.html"
+    form_class = PasswordChangeForm
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.url = reverse("accounts:change_password")
