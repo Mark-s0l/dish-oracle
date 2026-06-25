f2c73db07ee2c1bbaa8f7c70884ec49e36c9d4b5
Marksol <solodkovsasha.2005@gmail.com>
2026-06-25 11:10:49 +0300

    chore: translate urls.py comment to English

--- dish_oracle/urls.py
+++ dish_oracle/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
 
-# Для разработки: отдача медиафайлов через Django
+# For development: serving media files via Django
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
