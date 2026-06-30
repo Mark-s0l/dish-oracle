1e8286b149c4094f20bbdc5796c843c5d8d35e2b
Marksol <solodkovsasha.2005@gmail.com>
2026-06-30 13:54:40 +0300

    fix: use static() helper to resolve default avatar path

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,12 @@
-from django.db import models
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.templatetags.static import static
 
 
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(
-        upload_to="avatars/",
-        blank=True,
-        null=True
-    )
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
 
     def get_avatar(self):
         if self.avatar:
             return self.avatar.url
-        return "/static/icons/default_image_profile.svg"
+        return static("icons/default_image_profile.svg")
