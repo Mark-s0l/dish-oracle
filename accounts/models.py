ec2af646061dbbbcba348b870b433aaf5e9860dc
Marksol <solodkovsasha.2005@gmail.com>
2026-07-02 12:51:17 +0300

    fix: enforce unique email on CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.templatetags.static import static
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    email = models.EmailField(_("email address"), unique=True)
 
     def get_avatar(self):
         if self.avatar:
