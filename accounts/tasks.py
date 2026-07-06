85a4731651cb14baebe8409908e884c2fd503788
Marksol <solodkovsasha.2005@gmail.com>
2026-07-06 12:52:59 +0300

    refactor: unify email sending into generic Celery task
    - replace per-purpose email tasks with single parameterized
      send_email_task(subject, message, recipient_list, log_context)
    - standardize log_context format across all call sites

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,22 +1,25 @@
+import logging
+
 from celery import shared_task
 from celery.utils.log import get_task_logger
+
 from accounts.utils.mailer import Mailer, MailerError
-import logging
 
 logger = logging.getLogger("accounts")
 
 mailer = Mailer()
 
+
 @shared_task
-def send_registration_email_task(user_id, user_email):
+def send_email_task(subject, message, recipient_list, log_context=""):
     try:
         mailer.send(
-            subject="Успешная регистрация",
-            message="Вы были успешно зарегистрированы! Если это были не вы, пожалуйста, напишите нам",
-            recipient_list=[user_email],
+            subject=subject,
+            message=message,
+            recipient_list=recipient_list,
         )
     except MailerError:
         logger.warning(
-            f"[SIGN_UP]: Failed to send registration notification email; user={user_id}",
+            f"[{log_context}]: Failed to send email; recipients={recipient_list}",
             exc_info=True,
         )
--- accounts/views.py
+++ accounts/views.py
@@ -3,20 +3,19 @@ import secrets
 
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import login, update_session_auth_hash
 from django.contrib.auth.hashers import check_password, make_password
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView, UpdateView
 
-from accounts.forms import ChangeEmailUser, EmailVerificationCode, ChangePasswordForm, SignUpUserForm
+from accounts.forms import (ChangeEmailUser, ChangePasswordForm,
+                            EmailVerificationCode, SignUpUserForm)
 from accounts.models import CustomUser
+from accounts.tasks import send_email_task
 from accounts.utils.cache_manager import CacheError, CacheManager
 from accounts.utils.mailer import Mailer, MailerError
-from django.contrib.auth import login
-
-from accounts.tasks import send_registration_email_task
 
 logger = logging.getLogger("accounts")
 
@@ -98,7 +97,7 @@ class ChangePasswordView(LoginRequiredMixin, FormView):
             messages.info(self.request, "Ошибка отправки письма. Попробуйте позже")
             return redirect("accounts:profile")
 
-        logger.info(f"[CHANGE_PASSWORD] User={
+        logger.info(f"[CHANGE_PASSWD] User={
                 self.request.user.id
                 } started the password change procedure")
 
@@ -143,15 +142,13 @@ class VerificationChangePassword(LoginRequiredMixin, FormView):
             )
             messages.info(self.request, "Слишком много ошибок, попробуйте позже")
 
-            try:
-                mailer.send(
-                    subject="Смена пароля",
-                    message="Кто-то пытался сменить пароль много раз подряд безуспешно.",
-                    recipient_list=[self.request.user.email],
-                )
-            except MailerError:
-                logger.error(f"[CHANGE_PASSWORD] Сouldn't send a warning message for user={self.request.user.id}")
-            
+            send_email_task.delay(
+                subject="Смена пароля",
+                message="Кто-то пытался сменить пароль много раз подряд безуспешно",
+                recipient_list=[self.request.user.email],
+                log_context="[CHANGE_PASSWD]",
+            )
+
             cache_manager.cache_del("password_change")
             return redirect("accounts:profile")
 
@@ -170,7 +167,7 @@ class VerificationChangePassword(LoginRequiredMixin, FormView):
                     "password_change_attempt", {"attempt": attempt}, timeout=3600
                 )
             except CacheError:
-                logger.error(f"[CHANGE_PASSWORD] Cache set error", exc_info=True)
+                logger.error(f"[CHANGE_PASSWD] Cache set error", exc_info=True)
                 messages.info(self.request, "Произошла ошибка. Попробуйте позже")
                 return redirect("accounts:profile")
 
@@ -186,18 +183,16 @@ class VerificationChangePassword(LoginRequiredMixin, FormView):
         update_session_auth_hash(self.request, self.request.user)
         cache_manager.cache_del("password_change")
 
-        try:
-            mailer.send(
-                subject="Успешная смена пароля",
-                message="Пароль на учетной записи успешно изменен. Если это делали не вы, пожалуйста ответьте на это письмо",
-                recipient_list=[self.request.user.email],
-            )
-        except MailerError:
-            logger.error(f"[CHANGE_PASSWD] Failed to send security notice user={self.request.user.id}")
+        send_email_task.delay(
+            subject="Успешная смена пароля",
+            message="Пароль на учетной записи успешно изменен. Если это делали не вы, пожалуйста ответьте на это письмо",
+            recipient_list=[self.request.user.email],
+            log_context="[CHANGE_PASSWD]",
+        )
 
         messages.info(self.request, "Пароль успешно изменен")
         logger.info(
-            f"[CHANGE_PASSWORD] User={self.request.user.id} successfully changed the password"
+            f"[CHANGE_PASSWD] User={self.request.user.id} successfully changed the password"
         )
         return super().form_valid(form)
 
@@ -210,6 +205,10 @@ class SignUpUser(FormView):
     def form_valid(self, form):
         user = form.save()
         login(self.request, user, backend="accounts.backends.EmailBackend")
-        send_registration_email_task.delay(user.id, user.email)
+        send_email_task.delay(
+            subject="Успешная регистрация",
+            message="Вы были успешно зарегистрированы! Если это были не вы, пожалуйста, напишите нам",
+            recipient_list=[user.email],
+            log_context="SIGN_UP",
+        )
         return super().form_valid(form)
-
